affad3214b4f02b616b4e69070e877f4403b9b83
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-11-11 14:29:19 +0100

    done with the display of the registered teacher and student

--- app/links/[name]/page.js
+++ app/links/[name]/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import moment from "moment/moment";
 
 export default function Lists({ params: { name } }) {
   const [data, setData] = useState([]);
@@ -19,7 +20,7 @@ export default function Lists({ params: { name } }) {
         .then((response) => response.json())
         .then((response) => {
           console.log("data", response);
-          setData(response);
+          setData(response?.teachersData);
         });
     } catch (err) {
       console.log(err);
@@ -34,10 +35,35 @@ export default function Lists({ params: { name } }) {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="flex justify-center item-center">
-        <h2 className="text-white text-3xl">
+      <div className="flex flex-col justify-center item-center">
+        <h2 className="text-white text-3xl lg:mt-10">
           List of {name}s that have completed their registration details
         </h2>
+        {data.map((item, i) => (
+          <>
+            <div
+              className="w-[300px] lg:w-[400px] mt-[2rem] group rounded-lg border border-grey-300 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:mt-[5rem]"
+              key={i}
+            >
+              <p className="text-2xl text-white">{i + 1}.</p>
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                {item?.title} &nbsp; {item?.firstName + " " + item?.surname}{" "}
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 lg:text-lg`}>
+                national Id: {item?.nationalId}
+              </p>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 lg:text-lg`}>
+                teacher's number: {item?.teacherNumber}
+              </p>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 lg:text-lg`}>
+                date of birth: {moment(item?.dateOfBirth).format("MMM Do YYYY")}
+              </p>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 lg:text-lg`}>
+                salary: {item?.salary}
+              </p>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
